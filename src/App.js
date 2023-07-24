e9f82c4c025e79e405f15584a3a0d084dbf2a9ce
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2023-07-24 22:11:25 +0530

    Adding the Sort Actions

--- src/App.js
+++ src/App.js
@@ -85,13 +85,39 @@ function Form({ onAddItems, onDeleteItem }) {
 }
 
 function PackingList({ items, ...rest }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item key={item.id} item={item} {...rest} />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
